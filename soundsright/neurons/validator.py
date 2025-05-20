07f895b5114b244e29a2305e644a5f7afe58ee1c
m4k1-dev <m4k1_m4k1@proton.me>
2025-05-20 17:28:55 +0900

    change dataset size to 300

--- soundsright/neurons/validator.py
+++ soundsright/neurons/validator.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     
     parser.add_argument(
         "--dataset_size",
-        default=250,
+        default=300,
         type=int,
         help="Size of evaluation dataset."
     )
