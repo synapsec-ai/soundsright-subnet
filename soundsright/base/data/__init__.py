a173b06b9c9107b2e1c2217db3751f454a9481f9
m4k1-dev <155216315+m4k1-dev@users.noreply.github.com>
2025-05-22 21:45:34 +0900

    v1.2.1 (#7)
    - Fixed import error, found courtesy of @superman195

--- soundsright/base/data/__init__.py
+++ soundsright/base/data/__init__.py
@@ -8,5 +8,6 @@ from .tts import TTSHandler
 
 from .generate import (
     reset_all_data_directories,
-    create_noise_and_reverb_data_for_all_sampling_rates
+    create_noise_and_reverb_data_for_all_sampling_rates,
+    generate_dataset_for_miner
 )
