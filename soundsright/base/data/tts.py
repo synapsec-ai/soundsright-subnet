d2bbdddb12b50aa780b039464690ef4087264cfb
m4k1-dev <m4k1_m4k1@proton.me>
2025-06-14 22:45:44 +0900

    deterministic text in dataset generation

--- soundsright/base/data/tts.py
+++ soundsright/base/data/tts.py
@@ -3,6 +3,7 @@ import os
 from openai import OpenAI
 import librosa
 import soundfile as sf
+from wonderwords import RandomSentence
 from typing import List
 
 from soundsright.base.utils import subnet_logger 
@@ -27,36 +28,14 @@ class TTSHandler:
         self.openai_client = OpenAI(api_key=api_key)
         self.openai_voices = ['alloy','echo','fable','onyx','nova','shimmer']
         self.log_level = log_level
-
-    def _generate_prompt(self) -> str:
-        topic = random.choice(tuple(TOPICS)).lower()
-        emotion = random.choice(tuple(EMOTIONS)).lower()
-        prompt_selection = random.randint(0,3)
-        
-        if prompt_selection == 0:
-            return f"You are a somebody who knows a lot of facts. Please provide an output related to the following topic: '{topic}' with a tone of '{emotion}'. Do not start your response with the topic. Respond in English. Do not include anything at the start, or the end, but just the sentence, as your reply will be formatted into a larger block of text and it needs to flow smoothly."
-            
-        elif prompt_selection == 1:
-            return f"You are a somebody who asks a lot of thought-provoking questions. Please provide an output related to the following topic: '{topic}' with a tone of '{emotion}'. Do not start your response with the topic. Respond in English. Do not include anything at the start, or the end, but just the sentence, as your reply will be formatted into a larger block of text and it needs to flow smoothly."
-    
-        return f"You are a somebody who provides commentary on a wide variety of topics. Please provide an output related to the following topic: '{topic}' with a tone of: '{emotion}'. Do not start your response with the topic. Respond in English. Do not include anything at the start, or the end, but just the sentence, as your reply will be formatted into a larger block of text and it needs to flow smoothly."
+        self.rs = RandomSentence()
 
     # Generates unique sentences for TTS 
     def _generate_random_sentence(self, n: int=8) -> str:
-        prompt = self._generate_prompt()
-        messages = [
-            {
-                "role":"system",
-                "content":"You will be asked to partake in different forms of conversation. Do not start your response with the topic. Respond in English. Do not include anything at the start, or the end, but just the sentence, as your reply will be formatted into a larger block of text and it needs to flow smoothly."
-            },
-            {
-                "role":"user",
-                "content":prompt
-            }
-        ]
-        
-        completion = self.openai_client.chat.completions.create(model='gpt-4o', messages=messages)
-        return completion.choices[0].message.content
+        output = ""
+        for _ in range(0,2):
+            output += self.rs.sentence() + " "
+        return output
         
     # Generates one output TTS file at correct sample rate
     def _do_single_openai_tts_query(self, tts_file_path: str, sample_rate: int, voice: str = 'random'):
@@ -127,7 +106,9 @@ class TTSHandler:
             )
 
     # Create TTS dataset of length n for all sample rates
-    def create_openai_tts_dataset_for_all_sample_rates(self, n:int):
+    def create_openai_tts_dataset_for_all_sample_rates(self, n:int, seed:int):
+
+        random.seed(seed)
             
         for sample_rate in self.sample_rates: 
             self.create_openai_tts_dataset(
