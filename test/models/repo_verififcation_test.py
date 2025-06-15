051d3619fcf0c17384b69d49bdfa2537329098f4
m4k1-dev <m4k1_m4k1@proton.me>
2025-06-15 23:32:28 +0900

    bruh

--- test/models/repo_verififcation_test.py
+++ test/models/repo_verififcation_test.py
@@ -7,7 +7,7 @@ import pytest
     ("synapsecai", "SoundsRightModelTemplate", "main", False),
     ("synapsecai", "SoundsRightModelTemplate", "191137582e8f5b67a0d5fee2f60a08a8a46f3d98", True),
     ("synapsecai", "SoundsRightModelTemplate", "70a8146e69bc18074f6b73f9ea4bf068aaa3ec8b", True),
-    ("synapsecai", "SoundsRightModelTemplate", "70a8146e69bc18074f6b73f9ea4bf068aaa3ec8c", True),
+    ("synapsecai", "SoundsRightModelTemplate", "70a8146e69bc18074f6b73f9ea4bf068aaa3ec8c", False),
 ])
 def test_repo_verification(namespace, name, revision, outcome):
 
