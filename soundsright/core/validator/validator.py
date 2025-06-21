54d6db5361ffa91854c19f0ccc4543a636afa938
m4k1-dev <m4k1_m4k1@proton.me>
2025-06-22 00:06:13 +0900

    adjustment

--- soundsright/core/validator/validator.py
+++ soundsright/core/validator/validator.py
@@ -1533,7 +1533,7 @@ class SubnetValidator(Base.BaseNeuron):
 
         try:
 
-            if (not self.first_run_through_of_the_day and self.check_if_time_to_benchmark()) or self.debug_mode:
+            if self.check_if_time_to_benchmark() and not self.first_run_through_of_the_day:
                 self.neuron_logger(
                     severity="DEBUG",
                     message=f"Not enough time in current competition to benchmark model for hotkey: {hotkey}."
