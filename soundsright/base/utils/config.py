427913a941b9ad9ecf733d3b840e6a588c284cab
m4k1-dev <m4k1_m4k1@proton.me>
2025-06-23 14:40:53 +0900

    init

--- soundsright/base/utils/config.py
+++ soundsright/base/utils/config.py
@@ -13,7 +13,7 @@ class ModuleConfig:
     def __init__(self):
 
         # Determine module code version
-        self.__version__ = "1.5.3"
+        self.__version__ = "1.6.0"
 
         # Determine the score version
         self.__score_version__ = "3"
