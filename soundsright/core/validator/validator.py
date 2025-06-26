8eae77d57e5175989d9407b6c66d4629334b2dbf
m4k1-dev <155216315+m4k1-dev@users.noreply.github.com>
2025-06-27 02:02:58 +0900

    v1.5.4 (#19)
    - Fixed bug where weights were sometimes negative

--- soundsright/core/validator/validator.py
+++ soundsright/core/validator/validator.py
@@ -1268,6 +1268,9 @@ class SubnetValidator(Base.BaseNeuron):
     async def commit_weights(self) -> None:
         """Sets the weights for the subnet"""
 
+        def filter_negative_weights(weights):
+            return [max(0, w) for w in weights]
+
         def normalize_weights_list(weights, max_value:int):
             if all(x==1 for x in weights):
                 return [(x/max_value) for x in weights]
@@ -1296,6 +1299,8 @@ class SubnetValidator(Base.BaseNeuron):
                 weights=weights,
                 max_value=max_value,
             )
+
+            weights = filter_negative_weights(weights = weights)
 
             if self.wc_prevention_protcool:
                 # Modify according to miner nonce avg
