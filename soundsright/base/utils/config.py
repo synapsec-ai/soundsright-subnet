e57948ef6534418c18d247aa39b540ea56f60b2f
m4k1-dev <m4k1_m4k1@proton.me>
2025-08-16 18:28:50 +0900

    cache reset in next update

--- soundsright/base/utils/config.py
+++ soundsright/base/utils/config.py
@@ -16,7 +16,7 @@ class ModuleConfig:
         self.__version__ = "1.7.0"
 
         # Determine the score version
-        self.__score_version__ = "4"
+        self.__score_version__ = "3"
 
         # Convert the version into a single integer
         self.__version_split__ = self.__version__.split(".")
