dcebfa64613aaad54ef8f54b422363de2066122e
m4k1-dev <m4k1_m4k1@proton.me>
2025-09-03 02:01:34 +0900

    adjustment

--- test/models/validation_test.py
+++ test/models/validation_test.py
@@ -89,7 +89,7 @@ def test_get_model_content_hash_bulk(model_id, revision):
     ("synapsecai", "SoundsRightModelTemplate", "191137582e8f5b67a0d5fee2f60a08a8a46f3d98", True),
     ("synapsecai", "SoundsRightModelTemplate", "763a9b1c598bd16648ffccc9b9c67c67b6aa3805", True),
     ("synapsecai", "SoundsRightModelTemplate", "191137582e8f5b67a0d5fee2f60a08a8a46f3d9", False),
-    ("synapsecai", "SoundsRightModelTemplate", "763a9b1c59d16648ffccc9b9c67c67b6aa3805", False),
+    ("synapsecai", "SoundsRightModelTemplate", "763a9b1c59d53916648ffccc9b9c67c67b6aa3805", False),
 ])
 def test_check_repo_exists(namespace, name, revision, outcome):
     assert Models.check_repo_exists(namespace=namespace,name=name,revision=revision) == outcome, f"Model existence check failed for {namespace}/{name}/{revision}"
